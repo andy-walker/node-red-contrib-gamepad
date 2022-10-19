dc4e2594e1be1de753ab385c7cd85715f0c45a1c
Andrew Walker <andrew.walker06@bbc.co.uk>
2022-10-19 17:56:58 +0100

    Fix outputs.

--- gamepad.js
+++ gamepad.js
@@ -9,22 +9,22 @@ module.exports = function(RED) {
         gamepad.on('button', button => {
             switch (button.number) {
                 case 0: // cross
-                    node.send(null, null, null, null, { payload: button.value }, null, null, null, null, null);
+                    node.send([null, null, null, null, { payload: button.value }, null, null, null, null, null]);
                     break;
                 case 1: // circle
-                    node.send(null, null, null, null, null, { payload: button.value }, null, null, null, null);
+                    node.send([null, null, null, null, null, { payload: button.value }, null, null, null, null]);
                     break;
                 case 2: // triangle
-                    node.send(null, null, null, null, null, null, { payload: button.value }, null, null, null);
+                    node.send([null, null, null, null, null, null, { payload: button.value }, null, null, null]);
                     break;
                 case 3: // square
-                    node.send(null, null, null, null, null, null, null, { payload: button.value }, null, null);
+                    node.send([null, null, null, null, null, null, null, { payload: button.value }, null, null]);
                     break;
                 case 4: // L1
-                    node.send(null, null, null, null, null, null, null, null, { payload: button.value }, null);
+                    node.send([null, null, null, null, null, null, null, null, { payload: button.value }, null]);
                     break;
                 case 5: // R1
-                    node.send(null, null, null, null, null, null, null, null, null, { payload: button.value });
+                    node.send([null, null, null, null, null, null, null, null, null, { payload: button.value }]);
                     break;       
             }
         });
@@ -32,16 +32,16 @@ module.exports = function(RED) {
         gamepad.on('axis', axis => {
             switch (axis.number) {
                 case 0: // left stick x-axis
-                    node.send({ payload: axis.value }, null, null, null, null, null, null, null, null, null);
+                    node.send([{ payload: axis.value }, null, null, null, null, null, null, null, null, null]);
                     break;
                 case 1: // left stick y-axis
-                    node.send(null, { payload: axis.value }, null, null, null, null, null, null, null, null);
+                    node.send([null, { payload: axis.value }, null, null, null, null, null, null, null, null]);
                     break;
                 case 3: // right stick x-axis
-                    node.send(null, null, { payload: axis.value }, null, null, null, null, null, null, null);
+                    node.send([null, null, { payload: axis.value }, null, null, null, null, null, null, null]);
                     break;
                 case 4: // right stick y-axis
-                    node.send(null, null, null, { payload: axis.value }, null, null, null, null, null, null);
+                    node.send([null, null, null, { payload: axis.value }, null, null, null, null, null, null]);
                     break;
             }
         });
