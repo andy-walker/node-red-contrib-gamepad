33055877a26a296238aa39aff1e7918ed78a5ddc
Andrew Walker <andrew.walker06@bbc.co.uk>
2022-10-23 18:29:58 +0100

    Add remaining buttons.

--- gamepad.js
+++ gamepad.js
@@ -1,47 +1,82 @@
 module.exports = function(RED) {
     function GamepadNode(config) {
-        RED.nodes.createNode(this, config);
 
         const node = this;
+
+        function send(index, value) {
+            let output = [];
+            output[index] = { payload: value };
+            node.send(output);
+        }
+
+        RED.nodes.createNode(this, config);
+
         const joystick = require('joystick');
         const gamepad = new joystick(0);
-        
+   
         gamepad.on('button', button => {
             switch (button.number) {
                 case 0: // cross
-                    node.send([null, null, null, null, { payload: button.value }, null, null, null, null, null]);
+                    send(4, button.value);
                     break;
                 case 1: // circle
-                    node.send([null, null, null, null, null, { payload: button.value }, null, null, null, null]);
+                    send(5, button.value);
                     break;
                 case 2: // triangle
-                    node.send([null, null, null, null, null, null, { payload: button.value }, null, null, null]);
+                    send(6, button.value);
                     break;
                 case 3: // square
-                    node.send([null, null, null, null, null, null, null, { payload: button.value }, null, null]);
+                    send(7, button.value);
                     break;
                 case 4: // L1
-                    node.send([null, null, null, null, null, null, null, null, { payload: button.value }, null]);
+                    send(8, button.value);
                     break;
                 case 5: // R1
-                    node.send([null, null, null, null, null, null, null, null, null, { payload: button.value }]);
-                    break;       
+                    send(9, button.value);
+                    break;
+                case 6: // L2
+                    send(12, button.value);
+                    break;
+                case 7: // R2
+                    send(13, button.value);
+                    break;
+                case 8: // share button
+                    send(16, button.value);
+                    break;
+                case 9: // options button
+                    send(17, button.value);
+                    break;
+                case 10: // playstation button
+                    send(18, button.value);
+                    break;
             }
         });
         
         gamepad.on('axis', axis => {
             switch (axis.number) {
                 case 0: // left stick x-axis
-                    node.send([{ payload: axis.value }, null, null, null, null, null, null, null, null, null]);
+                    send(0, axis.value);
                     break;
                 case 1: // left stick y-axis
-                    node.send([null, { payload: axis.value }, null, null, null, null, null, null, null, null]);
+                    send(1, axis.value);                    
+                    break;
+                case 2: // L2 axis data
+                    send(10, axis.value);                    
                     break;
                 case 3: // right stick x-axis
-                    node.send([null, null, { payload: axis.value }, null, null, null, null, null, null, null]);
+                    send(2, axis.value); 
                     break;
                 case 4: // right stick y-axis
-                    node.send([null, null, null, { payload: axis.value }, null, null, null, null, null, null]);
+                    send(3, axis.value); 
+                    break;
+                case 5: // R2 axis data
+                    send(11, axis.value);                    
+                    break;
+                case 6: // d-pad x-axis
+                    send(14, axis.value);         
+                    break;
+                case 7: // d-pad y-axis
+                    send(15, axis.value);         
                     break;
             }
         });
